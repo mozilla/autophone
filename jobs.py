2e7c3ec57fcbfc0b1619a02f753c56c73e203dbf
Bob Clary <bob@bclary.com>
2015-03-23 05:51:25 -0700

    Bug 1133580 - Autophone - implement new jobs format including new tests table in jobs.py, r=mcote.

--- jobs.py
+++ jobs.py
@@ -3,7 +3,6 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import datetime
-import json
 import logging
 import os
 import sqlite3
@@ -24,19 +23,34 @@ class Jobs(object):
         if not os.path.exists(self.filename):
             conn = self._conn()
             c = conn.cursor()
-            c.execute('create table jobs '
-                      '(created text, last_attempt text, build_url text, '
-                      'tests text, attempts int, device text)')
+            c.execute('create table jobs ('
+                      'id integer primary key, '
+                      'created text, '
+                      'last_attempt text, '
+                      'build_url text, '
+                      'build_id text, '
+                      'changeset text, '
+                      'tree text, '
+                      'revision text, '
+                      'revision_hash, '
+                      'enable_unittests int, '
+                      'attempts int, '
+                      'device text)')
+            c.execute('create table tests ('
+                      'id integer primary key, '
+                      'name text, '
+                      'guid text, '
+                      'jobid integer)')
             conn.commit()
 
     def report_sql_error(self, attempt, email_sent,
                          email_subject, email_body,
                          exception_message):
+        logger.exception(exception_message)
         if attempt > self.SQL_MAX_RETRIES and not email_sent:
             email_sent = True
             self.mailer.send(email_subject, email_body)
             logger.info('Sent mail notification about jobs database sql error.')
-            logger.exception(exception_message)
             time.sleep(self.SQL_RETRY_DELAY)
         return email_sent
 
@@ -66,6 +80,26 @@ class Jobs(object):
             attempt += 1
             try:
                 conn = self._conn()
+                conn.cursor().execute('delete from tests')
+                conn.commit()
+                break
+            except sqlite3.OperationalError:
+                email_sent = self.report_sql_error(attempt, email_sent,
+                                                   'Unable to clear all tests in '
+                                                   'jobs database.',
+                                                   'Please check the logs for '
+                                                   'full details.',
+                                                   'Attempt %d failed to delete '
+                                                   'all jobs database. Waiting '
+                                                   'for %d seconds.' %
+                                                   (attempt,
+                                                    self.SQL_RETRY_DELAY))
+        attempt = 0
+        email_sent = False
+        while True:
+            attempt += 1
+            try:
+                conn = self._conn()
                 conn.cursor().execute('delete from jobs')
                 conn.commit()
                 break
@@ -81,58 +115,115 @@ class Jobs(object):
                                                    (attempt,
                                                     self.SQL_RETRY_DELAY))
 
-    def new_job(self, build_url, tests=None, device=None):
+    def new_job(self, build_url, build_id=None, changeset=None, tree=None,
+                revision=None, revision_hash=None, tests=None,
+                enable_unittests=False, device=None):
+        logger.debug('jobs.new_job: %s %s %s %s %s %s %s %s %s' % (
+            build_url, build_id, changeset, tree, revision, revision_hash,
+            tests, enable_unittests, device))
         if not device:
             device = self.default_device
         now = datetime.datetime.now().isoformat()
-        attempt = 0
         email_sent = False
-        tests.sort()
-        stests = json.dumps(tests)
+        job_id = None
+        duplicate = False
+        test_names = []
+        for test in tests:
+            test_names.append(test.name)
+            # generate a new guid for the test
+            test.generate_guid()
+        test_names.sort()
+
         try:
-            count = self._conn().cursor().execute(
-                'select count(ROWID) from jobs where device=? and '
-                'build_url=? and tests=?',
-                (device,build_url,stests)).fetchone()[0]
+            conn = self._conn()
+            job_cursor = conn.cursor()
+            test_cursor = conn.cursor()
+            job_cursor.execute(
+                'select id from jobs where device=? and build_url=?',
+                (device, build_url))
+            job = job_cursor.fetchone()
+            while job:
+                job_id = job[0]
+                job_test_names = [
+                    test_row[0] for test_row in
+                    test_cursor.execute(
+                        'select name from tests where jobid=?',
+                        (job_id,))]
+                job_test_names.sort()
+                if test_names == job_test_names:
+                    duplicate = True
+                    break
+                job = job_cursor.fetchone()
         except sqlite3.OperationalError:
-            count = 0
-        if count > 0:
-            logger.warning('Not adding duplicate job: '
-                           'build: %s, device: %s, tests: %s' % (
-                               build_url, device, tests))
+            logger.exception('jobs.new_job')
+            pass
+
+        if duplicate:
+            logger.warning('jobs.new_job: Not adding duplicate job: '
+                           'build: %s, device: %s, test_names: %s' % (
+                               build_url, device, test_names))
             return
 
-        while True:
-            attempt += 1
-            try:
-                conn = self._conn()
-                conn.cursor().execute(
-                    'insert into jobs values (?, ?, ?, ?, 0, ?)',
-                    (now, None, build_url, stests, device))
-                conn.commit()
-                break
-            except sqlite3.OperationalError:
-                email_sent = self.report_sql_error(
-                    attempt, email_sent,
-                    'Unable to insert job into jobs database.',
-                    'Please check the logs for full details.',
-                    'Attempt %d failed to insert job (%s, %s, %s, %s, %s) into '
-                    'database. Waiting for %d seconds.' %
-                    (attempt, now, None,
-                     build_url, tests, device, self.SQL_RETRY_DELAY))
+        if not job_id:
+            attempt = 0
+            while True:
+                attempt += 1
+                try:
+                    conn = self._conn()
+                    job_cursor = conn.cursor()
+                    job_cursor.execute(
+                        'insert into jobs values '
+                        '(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0, ?)',
+                        (None, now, None, build_url, build_id, changeset, tree,
+                         revision, revision_hash, enable_unittests, device))
+                    conn.commit()
+                    job_id = job_cursor.lastrowid
+                    break
+                except sqlite3.OperationalError:
+                    email_sent = self.report_sql_error(
+                        attempt, email_sent,
+                        'Unable to insert job into jobs database.',
+                        'Please check the logs for full details.',
+                        'Attempt %d failed to insert job (%s, %s, %s) into '
+                        'database. Waiting for %d seconds.' %
+                        (attempt, build_url, device, test_names,
+                         self.SQL_RETRY_DELAY))
+
+        for test in tests:
+            attempt = 0
+            email_sent = False
+            while True:
+                attempt += 1
+                try:
+                    conn = self._conn()
+                    test_cursor = conn.cursor()
+                    test_cursor.execute(
+                        'insert into tests values (?, ?, ?, ?)',
+                        (None, test.name, test.job_guid, job_id))
+                    conn.commit()
+                    break
+                except sqlite3.OperationalError:
+                    email_sent = self.report_sql_error(
+                        attempt, email_sent,
+                        'Unable to insert job into jobs database.',
+                        'Please check the logs for full details.',
+                        'Attempt %d failed to insert job (%s, %s, %s, %s, %s, %s) into '
+                        'database. Waiting for %d seconds.' %
+                        (attempt, now, None, build_url, test_names,
+                         enable_unittests, device, self.SQL_RETRY_DELAY))
 
     def jobs_pending(self, device=None):
         if not device:
             device = self.default_device
         try:
             count = self._conn().cursor().execute(
-                'select count(ROWID) from jobs where device=?',
+                'select count(id) from jobs where device=?',
                 (device,)).fetchone()[0]
         except sqlite3.OperationalError:
             count = 0
         return count
 
-    def get_next_job(self, lifo=False, device=None):
+    def get_next_job(self, lifo=False, device=None, worker=None):
         if not device:
             device = self.default_device
         order = 'desc' if lifo else 'asc'
@@ -140,6 +231,13 @@ class Jobs(object):
         try:
             conn = self._conn()
             c = conn.cursor()
+            job_ids = [
+                job[0] for job in
+                c.execute('select id from jobs where device=? and attempts>=?',
+                          (device, self.MAX_ATTEMPTS))]
+            for job_id in job_ids:
+                c.execute('delete from tests where jobid=?',
+                          (job_id,))
             c.execute('delete from jobs where device=? and attempts>=?',
                       (device, self.MAX_ATTEMPTS))
             conn.commit()
@@ -147,12 +245,18 @@ class Jobs(object):
                      'created': job[1],
                      'last_attempt': job[2],
                      'build_url': job[3],
-                     'tests': job[4],
-                     'attempts': job[5],
-                     'istry': job[6]}
+                     'build_id': job[4],
+                     'changeset': job[5],
+                     'tree': job[6],
+                     'revision': job[7],
+                     'revision_hash': job[8],
+                     'enable_unittests': job[9],
+                     'attempts': job[10],
+                     'istry': job[11]}
                     for job in c.execute(
-                            'select ROWID as id,created,last_attempt,build_url,'
-                            'tests,attempts,instr(build_url,"try") as istry '
+                            'select id,created,last_attempt,build_url,'
+                            'build_id,changeset,tree,revision,revision_hash,'
+                            'enable_unittests,attempts,instr(build_url,"try") as istry '
                             'from jobs where device=? order by istry desc, '
                             'created %s' % order,
                             (device,))]
@@ -161,17 +265,94 @@ class Jobs(object):
             next_job = jobs[0]
             next_job['attempts'] += 1
             next_job['last_attempt'] = datetime.datetime.now().isoformat()
-            c.execute('update jobs set attempts=?, last_attempt=? where ROWID=?',
+            c.execute('update jobs set attempts=?, last_attempt=? where id=?',
                       (next_job['attempts'], next_job['last_attempt'],
                        next_job['id']))
             conn.commit()
-            if next_job['tests']:
-                next_job['tests'] = json.loads(next_job['tests'])
+            next_job['tests'] = []
+            test_rows = [
+                {'name': test_row[0], 'guid': test_row[1]}
+                 for test_row in
+                 c.execute('select name, guid from tests where jobid=?',
+                           (next_job['id'],))]
+            for test_row in test_rows:
+                # Convert the list of test names in the job to a list
+                # of test objects and update their guid values.
+                for test in worker.tests:
+                    if test.name == test_row['name']:
+                        test.job_guid = test_row['guid']
+                        next_job['tests'].append(test)
         except sqlite3.OperationalError:
+            logger.exception('jobs.get_next_job')
             next_job = None
+        if next_job:
+            logger.debug('jobs.get_next_job: %s' % next_job)
         return next_job
 
-    def job_completed(self, job_id):
+    def cancel_job(self, test_name, test_guid, device=None):
+        logger.debug('jobs.cancel_job: %s %s %s' % (test_name, test_guid, device))
+        if not device:
+            device = self.default_device
+
+        email_sent = False
+        attempt = 0
+        while True:
+            attempt += 1
+            logger.debug('jobs.cancel_job: attempt %d to delete tests %s %s' % (attempt, test_name, test_guid))
+            try:
+                conn = self._conn()
+                c = conn.cursor()
+                # Collect the job ids corresponding to this test.
+                # There should be at most one such job id.
+                logger.debug('jobs.cancel_job: getting job_ids')
+                job_ids = [test_row[0]
+                           for test_row in c.execute(
+                                   'select distinct jobid from tests '
+                                   'where name=? and guid=?',
+                                   (test_name, test_guid))]
+                logger.debug('jobs.cancel_job: job_ids %s' % job_ids)
+                logger.debug('jobs.cancel_job: deleting test_name: %s, guid: %s' % (test_name, test_guid))
+                c.execute('delete from tests where '
+                          'name=? and guid=?',
+                          (test_name, test_guid))
+                conn.commit()
+                break
+            except sqlite3.OperationalError:
+                email_sent = self.report_sql_error(
+                    attempt, email_sent,
+                    'Unable to cancel job tests from jobs database.',
+                    'Please check the logs for full details.',
+                    'Attempt %d failed to delete test_name %s, '
+                    'test_guid %s from database. Waiting for %d '
+                    'seconds.' %
+                    (attempt, test_name, test_guid, self.SQL_RETRY_DELAY))
+        email_sent = False
+        attempt = 0
+        while True:
+            attempt += 1
+            logger.debug('jobs.cancel_job: attempt %d to delete jobs %s' % (attempt, job_ids))
+            try:
+                conn = self._conn()
+                c = conn.cursor()
+                for job_id in job_ids:
+                    logger.debug('jobs.cancel_job: delete job_id %s' % job_id)
+                    c.execute('delete from jobs where id=?',
+                              (job_id,))
+                conn.commit()
+                break
+            except sqlite3.OperationalError:
+                email_sent = self.report_sql_error(
+                    attempt, email_sent,
+                    'Unable to cancel job from jobs database.',
+                    'Please check the logs for full details.',
+                    'Attempt %d failed to delete test_name %s, '
+                    'test_guid %s from database. Waiting for %d '
+                    'seconds.' %
+                    (attempt, test_name, test_guid, self.SQL_RETRY_DELAY))
+        logger.debug('jobs.cancel_job: exit')
+
+    def test_completed(self, test_guid):
+        logger.debug('jobs.test_completed: %s' % test_guid)
         attempt = 0
         email_sent = False
         while True:
@@ -179,7 +360,46 @@ class Jobs(object):
             try:
                 conn = self._conn()
                 c = conn.cursor()
-                c.execute('delete from jobs where ROWID=?', (job_id,))
+                c.execute('delete from tests where guid=?', (test_guid,))
+                conn.commit()
+                break
+            except sqlite3.OperationalError:
+                email_sent = self.report_sql_error(
+                    attempt, email_sent,
+                    'Unable to delete completed test from jobs database.',
+                    'Please check the logs for full details.',
+                    'Attempt %d failed to delete completed test %s from '
+                    'database. Waiting for %d seconds.' %
+                    (attempt, test_guid, self.SQL_RETRY_DELAY))
+
+    def job_completed(self, job_id):
+        logger.debug('jobs.job_completed: %s' % job_id)
+        attempt = 0
+        email_sent = False
+        while True:
+            attempt += 1
+            try:
+                conn = self._conn()
+                c = conn.cursor()
+                c.execute('delete from tests where jobid=?', (job_id,))
+                conn.commit()
+                break
+            except sqlite3.OperationalError:
+                email_sent = self.report_sql_error(
+                    attempt, email_sent,
+                    'Unable to delete completed job tests from jobs database.',
+                    'Please check the logs for full details.',
+                    'Attempt %d failed to delete completed job %s from '
+                    'database. Waiting for %d seconds.' %
+                    (attempt, job_id, self.SQL_RETRY_DELAY))
+        attempt = 0
+        email_sent = False
+        while True:
+            attempt += 1
+            try:
+                conn = self._conn()
+                c = conn.cursor()
+                c.execute('delete from jobs where id=?', (job_id,))
                 conn.commit()
                 break
             except sqlite3.OperationalError:
