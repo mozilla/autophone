7e1beca7af430c4454c9f882e94b25d659f88965
Bob Clary <bob@bclary.com>
2017-05-26 10:18:03 -0700

    Bug 1367874 - work around failure to invoke Quitter on Android 4.0/4.2, r=self.

--- files/base/quitter.js
+++ files/base/quitter.js
@@ -1,9 +1,26 @@
+/*
+ * Call quitter to exit the app after the page load event fires.
+ *
+ * Android 4.0, 4.2 call the load event handler but do not
+ * cause the app to quit, therefore we have added an additional
+ * call to quit which will be executed after 5 seconds. This
+ * should be long enough period for the page to complete loading
+ * if it is going to do so.
+ */
 function quit()
 {
-  if (typeof(AutophoneQuitter) != "undefined") {
-    AutophoneQuitter.quit();
-  } else if (typeof(Quitter) != "undefined") {
-    Quitter.quit();
+  try {
+    if (typeof(AutophoneQuitter) != "undefined") {
+      AutophoneQuitter.quit();
+    } else if (typeof(Quitter) != "undefined") {
+      Quitter.quit();
+    } else {
+      dump('quit: No Quitter available!');
+    }
+  } catch(ex) {
+    dump('quit: Exception ' + ex);
   }
 }
-window.addEventListener("load", function () { setTimeout(quit, 1000); }, false);
+
+document.addEventListener("load", function () { setTimeout(quit, 1000); }, false);
+setTimeout(quit, 5000);
