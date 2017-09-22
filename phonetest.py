3b3091aa5f4ebfbb3874281dbb3c2b45ec0c053b
Bob Clary <bob@bclary.com>
2017-09-22 12:00:14 -0700

    Bug 1402392 - Autophone - Prevent Firefox opening privacy notification tab during tests, r=gbrown

--- phonetest.py
+++ phonetest.py
@@ -412,6 +412,8 @@ class PhoneTest(object):
                 'datareporting.healthreport.uploadEnabled': False,
                 'datareporting.policy.dataSubmissionEnabled': False,
                 'datareporting.policy.dataSubmissionPolicyBypassAcceptance': True,
+                'datareporting.policy.dataSubmissionPolicyBypassNotification', True,
+                'datareporting.policy.firstRunURL': "",
                 'dom.ipc.plugins.flash.subprocess.crashreporter.enabled': False,
                 'experiments.manifest.uri': 'http://localhost/experiments-dummy/manifest',
                 'extensions.autoDisableScopes': 0, # By default don't disable add-ons from any scope
